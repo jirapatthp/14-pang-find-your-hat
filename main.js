cee394855b55282df51ffbe9c66b351b2407e0ab
colddcereal <jirapatthepsurin@192.168.1.5>
2025-11-28 15:30:45 +0700

    update:step1 & step2

--- main.js
+++ main.js
@@ -23,11 +23,39 @@ let playing = true;
 
 // Print board
 function printBoard(board) {
-	console.clear(); // call console.clear() before print each move
-	console.log(board);
+	console.clear(); 
+	// call console.clear() before print each move ปริ้นอยู่ที่เดิม เพื่อความต่อเนื่องของเกมส์
+	board.forEach(row=> {
+		console.log(row.join(""));
+	});
+}
+function getInput(){
+const input = prompt("Which way? (w/a/s/d): ");
+if (!["w","a","s","d"].includes(input)){
+	console.log("Invalid input! PLease use w/a/s/d.");
+	return null;
+}
+return input;
+}
+
+function movePlayer(direction) {
+	if (direction === "w"){
+		playerRow -= 1;
+	}
+	else if (direction === "s"){
+		playerRow += 1;
+	}
+	else if (direction === "a"){
+		playerCol -= 1;
+	}
+	else if (direction === "d"){
+		playerCol -= 1;
+	}
 }
 
 // Game play loop
 printBoard(board);
-const input = prompt("Which way? (w/a/s/d): ");
+const input = getInput();
 console.log(input);
+// อธิบาย logic ว่า ถ้าw/a/s/d ทำอะไร ถ้าไม่ใช่ แสดงอะไร
+
